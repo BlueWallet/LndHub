a28eead0ab62b51f76f5a7113e36006a3fde47c1
lndhub-admin <lndhub-admin@iprobot.com>
2021-07-11 03:32:10 +0200

    move default in varible name to comment
    keep // default  0.* comment as reminder what defaults was

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let config = {
   enableUpdateDescribeGraph: false,
   postRateLimit: 100,
   rateLimit: 200,
-  defaultForwardReserveFee: 0.01,
-  defaultIntraHubFee: 0.003,
+  forwardReserveFee: 0.01, // default 0.01
+  intraHubFee: 0.003, // default 0.003
   bitcoind: {
     rpc: 'http://login:password@1.1.1.1:8332/wallet/wallet.dat',
   },
