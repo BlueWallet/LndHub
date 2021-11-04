7008138bdabd690a91712f9ce2603f3a1f03dd87
kiwiidb <kwinten.debacker@hotmail.com>
2021-11-04 18:17:13 +0100

    WIP: add route to create withdraw link

---
+++ class/Widr.js
@@ -0,0 +1,34 @@
+var crypto = require('crypto');
+
+const STATUS_UNCLAIMED = "unclaimed"
+const STATUS_PENDING = "pending"
+
+export class Widr {
+  /**
+   *
+   * @param {Redis} redis
+   */
+  constructor(redis, amount, userid) {
+    this._redis = redis;
+    this._userid = userid;
+    this.amount = amount;
+    this.status = STATUS_UNCLAIMED;
+    let buffer = crypto.randomBytes(10);
+    let secret = buffer.toString('hex');
+    this.secret = secret;
+  }
+
+  async saveWithdrawal() {
+    let withdrawal = {
+      userId: this._userid,
+      amount: this.amount,
+      status: this.status,
+      secret: this.secret
+    };
+    let key = 'withdrawal_link_' + this.secret;
+    await this._redis.set(key, JSON.stringify(withdrawal));
+    await this._redis.expire(key, 3600 * 24 * 7); // 1 week
+    return withdrawal;
+  }
+
+}
--- class/index.js
+++ class/index.js
@@ -2,3 +2,4 @@ export * from './User';
 export * from './Lock';
 export * from './Paym';
 export * from './Invo';
+export * from './Widr';
