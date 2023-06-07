c2ddc49d6c90b9426cfbe97a67ee8e2a8908f579
thespielplatz <informatics@gmx.net>
2023-06-07 12:00:15 +0200

    [TASK] add basic config infos to support dashboard

--- controllers/support.js
+++ controllers/support.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const mustache = require('mustache')
 const logger = require('../utils/logger')
 const qr = require('qr-image')
+const config = require('../config')
 
 router.get('/', function (req, res) {
     logger.log('/support', [req.id]);
@@ -14,6 +15,13 @@ router.get('/', function (req, res) {
         uris : ['Todo'],
         num_of_accounts: 'wip',
         num_of_sats: 'wip',
+        forwardReserveFee: config.forwardReserveFee,
+        forwardReserveFeeInPercent: (config.forwardReserveFee * 100.0).toFixed(2),
+        intraHubFee: config.intraHubFee,
+        intraHubFeeInPercent: (config.intraHubFee * 100.0).toFixed(4),
+        allowLightningPaymentToNode: config.allowLightningPaymentToNode,
+        accountCreationMode: config.accountCreationMode,
+        generateSafetyOnChainAddress: config.generateSafetyOnChainAddress,
     }
 
     return res.status(200).send(mustache.render(html, Object.assign({}, data)))
