09d984202ddf87962c7927f9bc3215c53010490a
thespielplatz <informatics@gmx.net>
2023-06-13 11:21:56 +0200

    [TASK] redesign support sidebar, to 'not overlap' on regular screens

--- controllers/support.js
+++ controllers/support.js
@@ -12,9 +12,17 @@ shared.supportDashboardShowAccounts = config.supportDashboardShowAccounts
 router.get('/', function (req, res) {
     logger.log('/support', [req.id]);
     res.setHeader('Content-Type', 'text/html')
-    let html = fs.readFileSync('./templates/support.html').toString('utf8')
 
-    return res.status(200).send(mustache.render(html, Object.assign({}, shared)))
+    const sidebarTpl = fs.readFileSync('./templates/sidebar.html').toString('utf8')
+    const sidebar = mustache.render(sidebarTpl, Object.assign({}, shared))
+
+    const supportTpl = fs.readFileSync('./templates/support.html').toString('utf8')
+
+    const html = mustache.render(supportTpl, Object.assign({
+        sidebar: sidebar
+    }, shared))
+ 
+    return res.status(200).send(html)
 })
 
 router.get('/account/:userId', function (req, res) {
@@ -22,9 +30,17 @@ router.get('/account/:userId', function (req, res) {
 
     logger.log('/account', [req.id])
     res.setHeader('Content-Type', 'text/html')
-    let html = fs.readFileSync('./templates/account.html').toString('utf8')
 
-    return res.status(200).send(mustache.render(html, Object.assign({}, shared)))
+    const sidebarTpl = fs.readFileSync('./templates/sidebar.html').toString('utf8')
+    const sidebar = mustache.render(sidebarTpl, Object.assign({}, shared))
+
+    const accountTpl = fs.readFileSync('./templates/account.html').toString('utf8')
+
+    const html = mustache.render(accountTpl, Object.assign({
+        sidebar: sidebar
+    }, shared))
+
+    return res.status(200).send(html)
 })
 
 module.exports = router
